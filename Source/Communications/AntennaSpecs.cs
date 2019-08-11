7b2c475ac460e5310e4f55a86cb7c0abcc5edea9
Mike <11480934+MikeOnTea@users.noreply.github.com>
2019-08-11 20:12:00 +0200

    Only set a strength >= 0 for ModuleConfigurableAntenna vessels

--- Source/Communications/AntennaSpecs.cs
+++ Source/Communications/AntennaSpecs.cs
@@ -7,14 +7,16 @@
         public double CommNetPower { get; set; }
         public float MinDataRate { get; set; }
         public float MaxDataRate { get; set; }
+        public bool Valid { get; set; }
 
-        public AntennaSpecs(float txUsedPower, float idlePower, double commNetPower, float minDataRate, float maxDataRate)
+        public AntennaSpecs(float txUsedPower, float idlePower, double commNetPower, float minDataRate, float maxDataRate, bool valid = true)
         {
             TxUsedPower = txUsedPower;
             IdlePower = idlePower;
             CommNetPower = commNetPower;
             MinDataRate = minDataRate;
             MaxDataRate = maxDataRate;
+            Valid = valid;
         }
     }
 }
